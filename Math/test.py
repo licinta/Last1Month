c52f1e1a565cf5ebe4e030cab4525e00a554d341
licinta <ferdosaghazi26@gmail.com>
2023-02-28 18:48:43 +0800

    2/28

--- Math/test.py
+++
@@ -1,9 +0,0 @@
-u=int(input())
-A,M=[],[]
-for i in range(n):
-    m,a=map(int,input().split())
-    A.append(a)
-    M.append(m)
-from ../Math/exCRT.py import exCRT
-crt=exCRT(A,M)
-print(crt.calc())
