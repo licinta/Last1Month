7feca0d6836cee024c8fe9d12f45e82c519a058e
licinta <ferdosaghazi26@gmail.com>
2023-03-01 18:15:05 +0800

    3/1

--- Last1Month/数学/公平组合游戏/Nim游戏-台阶.py
+++
@@ -1,9 +0,0 @@
-n=int(input())
-stones=list(map(int,input().split()))
-
-import itertools,functools 
-stones=itertools.compress(stones,[i%2 for i in range(1,n+1)])
-if functools.reduce(lambda x,y:x^y,stones)==0:
-    print("No")
-else:
-    print("Yes")
--- Last1Month/递归与递推/717.py
+++
@@ -1,9 +0,0 @@
-n=int(input())
-
-dp=[0 for i in range(100)]
-dp[1]=1
-for i in range(2,n):
-    dp[i]=dp[i-2]+dp[i-1]
-
-for i in range(n):
-    print(dp[i],end=' ')
