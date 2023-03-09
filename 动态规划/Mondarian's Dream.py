bd5b71333617483c8cab489ff333e9e29e0f6a47
licinta <ferdosaghazi26@gmail.com>
2023-03-09 19:45:55 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	动态规划/Mondarian's Dream.py

--- 动态规划/Mondarian's Dream.py
+++ 动态规划/Mondarian's Dream.py
@@ -1,3 +1,25 @@
+from functools import lru_cache
+
+def brk(temp):
+    f = False
+    l,r=0,0
+    while l<len(temp):
+        if temp[l]=='0':
+            r=l
+            while r<len(temp) and temp[r]=='0':
+                r+=1
+            count=r-l
+            if count%2==1:
+                f=True
+                break
+            else :
+                l=r
+        else:
+            l+=1
+
+    return f
+
+@lru_cache(None)
 def Mondarian(a, b):
     """
     the number of bricks which sticked out from the i-1-th column to i-th column
@@ -10,24 +32,28 @@ def Mondarian(a, b):
     """
     dp = [[0 for i in range(1 << a)] for j in range(b+1)]
     dp[0][0] = 1
-    for i in range(1 << a):
-        for column in range(1, b+1):
+    for column in range(1, b+1):
+        for i in range(1 << a):
+            if dp[column-1][i]==0:
+                continue
             for j in range(1 << a):
                 temp = i & j
                 if temp != 0:  # the brick has a maxium length of 2
                     continue
                 temp = i | j
-                temp = str(bin(temp))[2:]
-                f = False
-                for k in range(1, a+1, 2):
-                    if k*'0' in temp:
-                        f = True
-                        break
-                if f:  # no odd interval was permitted to appear in all proper state.
+                temp = str(bin(temp))[2:].rjust(a,'0')
+
+                if brk(temp):  # no odd interval was permitted to appear in all proper state.
                     continue
 
-                dp[column][i] += dp[column-1][j]
+                dp[column][j] += dp[column-1][i]
     return dp[b][0]
 
 
-print(Mondarian(2, 4))
+# print(Mondarian(2, 4))
+
+if __name__=="__main__":
+    a,b=map(int,input().split())
+    while a|b:
+        print(Mondarian(a,b))
+        a,b=map(int,input().split())
