4fb3cd03574dcebe11629efa5fb592aaa4676fc5
licinta <ferdosaghazi26@gmail.com>
2023-03-14 14:54:54 +0800

    Merge remote-tracking branch 'origin/main'

--- 数据结构/KMP.py
+++ 数据结构/KMP.py
@@ -1,8 +1,15 @@
 class KMP:
-    def __init__(self, pattern="aabaaac"):
+    def __init__(self, pattern="aabaaa"):
         self.pattern = pattern
         self.string = None
-        self.p = None
+        self.p = [0]  # the longest match between prefix and suffix
+        for i in range(1, len(self.pattern)):
+            j = self.p[i - 1]
+            while j and self.pattern[j] != self.pattern[i]:
+                print(j, i)
+                j = self.p[j - 1]
+            j += int(self.pattern[i] == self.pattern[j])
+            self.p.append(j)
         """
         constitude the p-array which will be used in fallback.
         aabaaac   abcabb
@@ -11,21 +18,25 @@ class KMP:
 
     def set_string(self, string):
         self.string = string
-        self.p = [0]  # the longest match between prefix and suffix
-        for i in range(1, len(self.string)):
-            j = self.p[i - 1]
-            while j and self.string[j] != self.string[i]:
-                print(j, i)
-                j = self.p[j - 1]
-            j += int(self.string[i] == self.string[j])
-            self.p.append(j)
-        print(self.p)
 
     def match(self):
+        i, j = 0, 0
+        while i < len(self.string):
+            if self.string[i] == self.pattern[j]:
+                while i < len(self.string) and j < len(self.pattern) and self.string[i] == self.pattern[j]:
+                    i += 1
+                    j += 1
+                if j == len(self.pattern):
+                    print(i - j)
+                    j = self.p[j - 1]
+                if i == len(self.string):
+                    print("out")
+                    return
+            else:
+                i += 1
         return
 
 
-kmp = KMP()
-kmp.set_string("aabaaa")
-kmp = KMP()
-kmp.set_string("abcabb")
+kmp = KMP("aabaaa")
+kmp.set_string("aabaaabaaa")
+kmp.match()
